92e602326cb3a3f3054b0d15b53cb19c2b3c8bc3
a17jespe <a17jespe@student.his.se>
2021-04-08 16:38:33 +0200

    Adapter connection causing crash - Line 118

--- app/src/main/java/com/example/networking/MainActivity.java
+++ app/src/main/java/com/example/networking/MainActivity.java
@@ -7,9 +7,11 @@ import android.os.AsyncTask;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.ListView;
+import android.widget.Toast;
 
 import java.io.BufferedReader;
 import java.io.IOException;
@@ -26,23 +28,6 @@ import java.util.Arrays;
 public class MainActivity extends AppCompatActivity {
 
 //    private ArrayList<Mountain> ;
-
-    private String[] MountainNames = {"Ettan","Tvåan","Trean"};
-    private String[] MoutainLocations = {"Norrut","Söderut","Västerut"};
-    private int[] MountainSizes = {90,270,360};
-
-    private ArrayList<String> listData = new ArrayList<>(Arrays.asList(MountainNames));
-
-    ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,R.layout.layout_list_mountains, R.id.layout_list_mountains_xml,listData);
-
-    ListView my_listview = (ListView) findViewById(R.id.my_listview);
-
-//    my_listview.setAdapter(ArrayAdapter);
-
-
-
-
-
 
 //    public class ArrayList<Mountain> {
 //
@@ -61,55 +46,83 @@ public class MainActivity extends AppCompatActivity {
 
 
 
-    @SuppressLint("StaticFieldLeak")
-    private class JsonTask extends AsyncTask<String, String, String> {
+//    @SuppressLint("StaticFieldLeak")
+//    private class JsonTask extends AsyncTask<String, String, String> {
+//
+//        private HttpURLConnection connection = null;
+//        private BufferedReader reader = null;
+//
+//        protected String doInBackground(String... params) {
+//            try {
+//                URL url = new URL(params[0]);
+//                connection = (HttpURLConnection) url.openConnection();
+//                connection.connect();
+//
+//                InputStream stream = connection.getInputStream();
+//                reader = new BufferedReader(new InputStreamReader(stream));
+//
+//                StringBuilder builder = new StringBuilder();
+//                String line;
+//                while ((line = reader.readLine()) != null && !isCancelled()) {
+//                    builder.append(line).append("\n");
+//                }
+//                return builder.toString();
+//            } catch (MalformedURLException e) {
+//                e.printStackTrace();
+//            } catch (IOException e) {
+//                e.printStackTrace();
+//            } finally {
+//                if (connection != null) {
+//                    connection.disconnect();
+//                }
+//                try {
+//                    if (reader != null) {
+//                        reader.close();
+//                    }
+//                } catch (IOException e) {
+//                    e.printStackTrace();
+//                }
+//            }
+//            return null;
+//        }
+//
+//        @Override
+//        protected void onPostExecute(String json) {
+//            Log.d("TAG", json);
+//
+//        }
+//    }
 
-        private HttpURLConnection connection = null;
-        private BufferedReader reader = null;
 
-        protected String doInBackground(String... params) {
-            try {
-                URL url = new URL(params[0]);
-                connection = (HttpURLConnection) url.openConnection();
-                connection.connect();
+    private String[] MountainNames = {"Ettan","Tvåan","Trean"};
+    private String[] MountainLocations = {"Norrut","Söderut","Västerut"};
+    private int[] MountainSizes = {90,270,360};
+    private ArrayList<String> listData = new ArrayList<>(Arrays.asList(MountainNames));
 
-                InputStream stream = connection.getInputStream();
-                reader = new BufferedReader(new InputStreamReader(stream));
-
-                StringBuilder builder = new StringBuilder();
-                String line;
-                while ((line = reader.readLine()) != null && !isCancelled()) {
-                    builder.append(line).append("\n");
-                }
-                return builder.toString();
-            } catch (MalformedURLException e) {
-                e.printStackTrace();
-            } catch (IOException e) {
-                e.printStackTrace();
-            } finally {
-                if (connection != null) {
-                    connection.disconnect();
-                }
-                try {
-                    if (reader != null) {
-                        reader.close();
-                    }
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
-            }
-            return null;
-        }
-
-        @Override
-        protected void onPostExecute(String json) {
-            Log.d("TAG", json);
-
-        }
-    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
+
+        ArrayAdapter<String> mountainAdapter =
+                new ArrayAdapter<String>(this, R.layout.layout_list_mountains, listData);
+
+        ListView thelistview = (ListView) findViewById(R.id.my_listview);
+
+//        String TAG = "Testing:";
+//
+//        for (int i = 0;i < 2; i++){
+//            Log.e(TAG, listData.get(i));
+//        }
+
+//        thelistview.setAdapter(mountainAdapter);
+
+//        my_listview.setOnItemClickListener(new AdapterView.OnItemClickListener());{
+//            @override
+//            public void onItemClick(AdapterView<?> parent, View view, int position, long id){
+//                Toast.makeText(getApplicationContext(),"Hello",Toast.LENGTH_SHORT).show();
+//            }
+//        }
 
 
         super.onCreate(savedInstanceState);
@@ -118,7 +131,7 @@ public class MainActivity extends AppCompatActivity {
         final Button button = findViewById(R.id.button_execute);
         button.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
-                new JsonTask().execute("https://wwwlab.iit.his.se/brom/kurser/mobilprog/dbservice/admin/getdataasjson.php?type=brom");
+//                new JsonTask().execute("https://wwwlab.iit.his.se/brom/kurser/mobilprog/dbservice/admin/getdataasjson.php?type=brom");
             }
         });
 
