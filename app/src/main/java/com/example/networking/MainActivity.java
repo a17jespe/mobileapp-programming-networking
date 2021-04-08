4f5d90a746c6125951b68c56b1d91766416934f6
a17jespe <a17jespe@student.his.se>
2021-04-08 17:03:36 +0200

    Resolved crash by moving two lines of code up a few rows...

--- app/src/main/java/com/example/networking/MainActivity.java
+++ app/src/main/java/com/example/networking/MainActivity.java
@@ -27,71 +27,53 @@ import java.util.Arrays;
 
 public class MainActivity extends AppCompatActivity {
 
-//    private ArrayList<Mountain> ;
 
-//    public class ArrayList<Mountain> {
-//
-//        private String ID;
-//        private String name;
-//        private String type;
-//        private String location;
-//        private String category;
-//        private Integer size;
-//        private Integer cost;
-//        private String img;
-//        private String aux;
-//
-//    }
+    @SuppressLint("StaticFieldLeak")
+    private class JsonTask extends AsyncTask<String, String, String> {
 
+        private HttpURLConnection connection = null;
+        private BufferedReader reader = null;
 
+        protected String doInBackground(String... params) {
+            try {
+                URL url = new URL(params[0]);
+                connection = (HttpURLConnection) url.openConnection();
+                connection.connect();
 
+                InputStream stream = connection.getInputStream();
+                reader = new BufferedReader(new InputStreamReader(stream));
 
-//    @SuppressLint("StaticFieldLeak")
-//    private class JsonTask extends AsyncTask<String, String, String> {
-//
-//        private HttpURLConnection connection = null;
-//        private BufferedReader reader = null;
-//
-//        protected String doInBackground(String... params) {
-//            try {
-//                URL url = new URL(params[0]);
-//                connection = (HttpURLConnection) url.openConnection();
-//                connection.connect();
-//
-//                InputStream stream = connection.getInputStream();
-//                reader = new BufferedReader(new InputStreamReader(stream));
-//
-//                StringBuilder builder = new StringBuilder();
-//                String line;
-//                while ((line = reader.readLine()) != null && !isCancelled()) {
-//                    builder.append(line).append("\n");
-//                }
-//                return builder.toString();
-//            } catch (MalformedURLException e) {
-//                e.printStackTrace();
-//            } catch (IOException e) {
-//                e.printStackTrace();
-//            } finally {
-//                if (connection != null) {
-//                    connection.disconnect();
-//                }
-//                try {
-//                    if (reader != null) {
-//                        reader.close();
-//                    }
-//                } catch (IOException e) {
-//                    e.printStackTrace();
-//                }
-//            }
-//            return null;
-//        }
-//
-//        @Override
-//        protected void onPostExecute(String json) {
-//            Log.d("TAG", json);
-//
-//        }
-//    }
+                StringBuilder builder = new StringBuilder();
+                String line;
+                while ((line = reader.readLine()) != null && !isCancelled()) {
+                    builder.append(line).append("\n");
+                }
+                return builder.toString();
+            } catch (MalformedURLException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
+            } finally {
+                if (connection != null) {
+                    connection.disconnect();
+                }
+                try {
+                    if (reader != null) {
+                        reader.close();
+                    }
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(String json) {
+            Log.d("TAG", json);
+
+        }
+    }
 
 
     private String[] MountainNames = {"Ettan","Tvåan","Trean"};
@@ -102,20 +84,21 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
 
         ArrayAdapter<String> mountainAdapter =
                 new ArrayAdapter<String>(this, R.layout.layout_list_mountains, listData);
 
         ListView thelistview = (ListView) findViewById(R.id.my_listview);
-
+        thelistview.setAdapter(mountainAdapter);
 //        String TAG = "Testing:";
 //
 //        for (int i = 0;i < 2; i++){
 //            Log.e(TAG, listData.get(i));
 //        }
 
-//        thelistview.setAdapter(mountainAdapter);
+
 
 //        my_listview.setOnItemClickListener(new AdapterView.OnItemClickListener());{
 //            @override
@@ -125,13 +108,10 @@ public class MainActivity extends AppCompatActivity {
 //        }
 
 
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-
         final Button button = findViewById(R.id.button_execute);
         button.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
-//                new JsonTask().execute("https://wwwlab.iit.his.se/brom/kurser/mobilprog/dbservice/admin/getdataasjson.php?type=brom");
+                new JsonTask().execute("https://wwwlab.iit.his.se/brom/kurser/mobilprog/dbservice/admin/getdataasjson.php?type=brom");
             }
         });
 
--- app/src/main/java/com/example/networking/Mountain.java
+++ app/src/main/java/com/example/networking/Mountain.java
@@ -1,5 +1,20 @@
 package com.example.networking;
 
 public class Mountain {
-    
+
+
+        private String ID;
+        private String name;
+        private String type;
+        private String location;
+        private String category;
+        private Integer size;
+        private Integer cost;
+        private String img;
+        private String aux;
+
+        public Mountain (String ID, String name, String size){
+
+        }
+
 }
